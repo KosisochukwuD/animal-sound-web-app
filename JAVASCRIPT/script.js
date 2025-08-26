1419a240930516ff803173aef0b042dd8039c529
KosisochukwuD <kosiokechukwu9@gmail.com>
2025-08-26 15:23:15 +0100

    animal sound and noise

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,17 @@
+const animals = document.querySelectorAll('.box img');
+
+animals.forEach((animal) => {
+  animal.addEventListener('click', () => {
+    // Stop all other audios first
+    document.querySelectorAll('audio').forEach(aud => {
+      aud.pause();
+      aud.currentTime = 0; // reset position
+    });
+
+    // Get only the audio inside the clicked box
+    const audio = animal.parentElement.querySelector('audio');
+
+    // Play it
+    audio.play();
+  });
+});
